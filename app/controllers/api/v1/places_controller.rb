e5faf5279c084ffe920d0fef4e08300075555efb
sylvia boyani <sylvia.boyani@student.moringaschool.com>
2022-10-14 13:09:53 +0300

    edit controller actions

--- app/controllers/api/v1/places_controller.rb
+++ app/controllers/api/v1/places_controller.rb
@@ -1,51 +1,51 @@
 class Api::V1::PlacesController < ApplicationController
-  # before_action :set_place, only: %i[ show update destroy ]
+  before_action :set_place, only: %i[ show update destroy ]
 
-  # # GET /places
-  # def index
-  #   @places = Place.all
+  # GET /places
+  def index
+    @places = Place.all
 
-  #   render json: @places
-  # end
+    render json: @places
+  end
 
-  # # GET /places/1
-  # def show
-  #   render json: @place
-  # end
+  # GET /places/1
+  def show
+    render json: @place
+  end
 
-  # # POST /places
-  # def create
-  #   @place = Place.new(place_params)
+  # POST /places
+  def create
+    @place = Place.new(place_params)
 
-  #   if @place.save
-  #     render json: @place, status: :created, location: @place
-  #   else
-  #     render json: @place.errors, status: :unprocessable_entity
-  #   end
-  # end
+    if @place.save
+      render json: @place, status: :created, location: @place
+    else
+      render json: @place.errors, status: :unprocessable_entity
+    end
+  end
 
-  # # PATCH/PUT /places/1
-  # def update
-  #   if @place.update(place_params)
-  #     render json: @place
-  #   else
-  #     render json: @place.errors, status: :unprocessable_entity
-  #   end
-  # end
+  # PATCH/PUT /places/1
+  def update
+    if @place.update(place_params)
+      render json: @place
+    else
+      render json: @place.errors, status: :unprocessable_entity
+    end
+  end
 
-  # # DELETE /places/1
-  # def destroy
-  #   @place.destroy
-  # end
+  # DELETE /places/1
+  def destroy
+    @place.destroy
+  end
 
-  # private
-  #   # Use callbacks to share common setup or constraints between actions.
-  #   def set_place
-  #     @place = Place.find(params[:id])
-  #   end
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_place
+      @place = Place.find(params[:id])
+    end
 
-  #   # Only allow a list of trusted parameters through.
-  #   def place_params
-  #     params.require(:place).permit(:title, :image_url, :body)
-  #   end
+    # Only allow a list of trusted parameters through.
+    def place_params
+      params.require(:place).permit(:title, :image_url, :body)
+    end
 end
