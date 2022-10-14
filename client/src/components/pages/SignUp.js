9dca0c07c30baeb536532cfa4a67d27016f07a3a
sylvia boyani <sylvia.boyani@student.moringaschool.com>
2022-10-14 15:43:41 +0300

    add signup code

--- client/src/components/pages/SignUp.js
+++ client/src/components/pages/SignUp.js
@@ -1,9 +1,162 @@
-import React from 'react';
-import '../../App.css';
+// import React from 'react';
+// import '../../App.css';
 
-export default function SignUp() {
-  return <h1 className='sign-up'>
-     <video src={'./videos/Jump.mp4' }autoPlay loop muted />
-    Email me to book a spot on the next adventure. Now!</h1>;
+// export default function SignUp() {
+//   return <h1 className='sign-up'>
+//      <video src={'./videos/Jump.mp4' }autoPlay loop muted />
+//     Email me to book a spot on the next adventure. Now!</h1>;
 
-}
+// }
+
+import './App.css';
+  import React, { useState } from 'react';
+  import Home from './components/Home';
+  import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+  import { faUser } from '@fortawesome/free-solid-svg-icons';
+  import { faLock } from '@fortawesome/free-solid-svg-icons';
+  
+  function SignUp() {  
+  const [hide, sethide] = useState(true);
+  const [show, setshow] = useState(true);
+  const validateForm = (event) => {
+    event.preventDefault();
+    const input1 = document.querySelector("#input1");
+    const input2 = document.querySelector("#input2");
+    const status = document.querySelector(".status");
+  
+    let validation = "Please fill-in the above fields";
+    let emailVal = "Email validation error must include @ .com TryAgain!";
+    let passVal = "Password char must not be less than 8";
+    if(!input1.value) {
+      console.warn("validation error");
+      status.classList.add("active");
+      status.innerHTML = `${validation}`;
+    }else{
+      emailValidate();
+    }
+    function emailValidate() { 
+      let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+      if(!input1.value.match(pattern)){
+        console.warn("pattern failed")
+        status.classList.add("active");
+        status.innerHTML = `${emailVal}`;
+      }else{
+        console.log("input1 validated");
+        status.classList.remove("active");
+        input1.classList.add("valid");
+      }
+     }
+     if(!input2.value){
+        console.warn("validation error");
+        status.classList.add("active");
+        status.innerHTML = `${validation}`;
+     }else{
+       passwordValidate();
+     }
+     function passwordValidate() {
+       if(input2.value.length < 8){
+         console.warn("Password char must not be less than 8");
+         status.classList.add("active");
+        status.innerHTML = `${passVal}`;
+       }else{
+        console.log("input2 validated");
+        status.classList.remove("active");
+        input2.classList.add("valid");
+       }
+     }
+     if(input1.classList.contains("valid") && input2.classList.contains("valid")){
+       console.log('submitted');
+       setTimeout(() => {
+        //  window.location.href = 'AirbnbHome';
+        let container = document.querySelector(".container");
+        let wrapper = document.querySelector(".wrapper");
+        sethide(!hide + wrapper.classList.add("hide"));
+        setshow(!show + container.classList.add("show"));
+  
+  
+       }, 1000);
+     }
+    }
+    const handleMail = () => {
+      const input1 = document.querySelector("#input1");
+      const status = document.querySelector(".status");
+      let validation = "Please fill-in the above fields";
+      let emailVal = "Email validation error must include @ .com TryAgain!";
+    if(!input1.value) {
+      console.warn("validation error");
+      status.classList.add("active");
+      status.innerHTML = `${validation}`;
+    }else{
+      emailValidate();
+    }
+    function emailValidate() { 
+      let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+      if(!input1.value.match(pattern)){
+        console.warn("pattern failed")
+        status.classList.add("active");
+        status.innerHTML = `${emailVal}`;
+      }else{
+        console.log("input1 validated");
+        status.classList.remove("active");
+      }
+     }
+    }
+    const handlePassword  = () => {
+      const input2 = document.querySelector("#input2");
+      const status = document.querySelector(".status");
+  
+      let validation = "Please fill-in the above fields";
+      let passVal = "Password char must not be less than 8";
+       if(!input2.value){
+          console.warn("validation error");
+          status.classList.add("active");
+          status.innerHTML = `${validation}`;
+       }else{
+         passwordValidate();
+       }
+       function passwordValidate() {
+         if(input2.value.length < 8){
+           console.warn("Password char must not be less than 8");
+           status.classList.add("active");
+          status.innerHTML = `${passVal}`;
+         }else{
+          console.log("input2 validated");
+          status.classList.remove("active");
+         }
+       }
+    }
+    return (
+      <div className="App">
+        <Home />
+        <div className="wrapper">
+          <div className="content">
+            <div className="logo">
+              
+            </div>
+            <div className="c1">
+              <span>Become an member & be on flick always!</span>
+            </div>
+            <div className="form">
+              <div className="status"></div>
+              <form action="#" onSubmit={validateForm}>
+                <div className="eInput">
+                <FontAwesomeIcon className='icon' icon={faUser} />
+                  <input id='input1' type="text" placeholder='Enter Email Address' onKeyUp={handleMail}/>
+                </div>
+                <div className="eInput">
+                <FontAwesomeIcon className='icon' icon={faLock} />
+                  <input id='input2' type="password" placeholder='Enter Password' onKeyUp={handlePassword}/>
+                </div>
+                <div className="submit">
+                  <button type="submit">Submit</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
+  export default SignUp;
+
