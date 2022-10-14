9801e9c731ae8101ffd9bb55620e2781858d4b13
sylvia boyani <sylvia.boyani@student.moringaschool.com>
2022-10-14 16:30:32 +0300

    update

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 import '../../App.css';
 import Cards from '../Cards';
 import HeroSection from '../HeroSection';
-import Footer from '../Footer';
+import Footer from '../Footer'
 
 function Home() {
   return (
