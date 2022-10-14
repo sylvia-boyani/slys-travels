9584f6424d92f263fac6239e7a2cecbcf82ec589
sylvia boyani <sylvia.boyani@student.moringaschool.com>
2022-10-14 16:29:37 +0300

    edit components

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,17 +1,17 @@
-// import React from 'react';
-// import '../../App.css';
-// import Cards from '../Cards';
-// import HeroSection from '../HeroSection';
-// import Footer from '../Footer';
+import React from 'react';
+import '../../App.css';
+import Cards from '../Cards';
+import HeroSection from '../HeroSection';
+import Footer from '../Footer';
 
-// function Home() {
-//   return (
-//     <>
-//       <HeroSection />
-//       <Cards />
-//       <Footer />
-//     </>
-//   );
-// }
+function Home() {
+  return (
+    <>
+      <HeroSection />
+      <Cards />
+      <Footer />
+    </>
+  );
+}
 
-// export default Home;
+export default Home;
